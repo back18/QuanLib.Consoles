4546a2a3cb0887446d00eab3de161a52232818d8
Quan <2200191123@qq.com>
2024-04-28 13:06:29 +0800

    添加类 FontColor 和 ConsoleText

---
+++ QuanLib.Consoles/ConsoleText.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Consoles
+{
+    public readonly struct ConsoleText
+    {
+        public ConsoleText()
+        {
+            Text = string.Empty;
+            FontColor = FontColor.Default;
+        }
+
+        public ConsoleText(string text)
+        {
+            ArgumentNullException.ThrowIfNull(text, nameof(text));
+
+            Text = text;
+            FontColor = FontColor.Default;
+        }
+
+        public ConsoleText(string text, FontColor fontColor)
+        {
+            ArgumentNullException.ThrowIfNull(text, nameof(text));
+
+            Text = text;
+            FontColor = fontColor;
+        }
+
+        public string Text { get; }
+
+        public FontColor FontColor { get; }
+
+        public readonly static ConsoleText SpaceOfDefaultColor = new(" ", FontColor.Default);
+
+        public readonly static ConsoleText SpaceOfCurrentColor = new(" ", FontColor.Current);
+
+        public readonly static ConsoleText NewLineOfDefaultColor = new("\n", FontColor.Default);
+
+        public readonly static ConsoleText NewLineOfCurrentColor = new("\n", FontColor.Current);
+
+        public void WriteToConsole()
+        {
+            FontColor current = FontColor.Current;
+            FontColor.SetToConsole();
+            Console.Write(Text);
+            current.SetToConsole();
+        }
+
+        public void WriteLineToConsole()
+        {
+            WriteToConsole();
+            Console.WriteLine();
+        }
+
+        public static void Write(string text, FontColor fontColor)
+        {
+            new ConsoleText(text, fontColor).WriteToConsole();
+        }
+
+        public static void WriteLine(string text, FontColor fontColor)
+        {
+            new ConsoleText(text, fontColor).WriteLineToConsole();
+        }
+    }
+}
---
+++ QuanLib.Consoles/FontColor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Consoles
+{
+    public readonly struct FontColor
+    {
+        public FontColor()
+        {
+            BackgroundColor = DefaultBackgroundColor;
+            ForegroundColor = DefaultForegroundColor;
+        }
+
+        public FontColor(ConsoleColor backgroundColor, ConsoleColor foregroundColor)
+        {
+            BackgroundColor = backgroundColor;
+            ForegroundColor = foregroundColor;
+        }
+
+        public const ConsoleColor DefaultBackgroundColor = ConsoleColor.Black;
+
+        public const ConsoleColor DefaultForegroundColor = ConsoleColor.White;
+
+        public static readonly FontColor Default = new();
+
+        public static FontColor Current => new(Console.BackgroundColor, Console.ForegroundColor);
+
+        public ConsoleColor BackgroundColor { get; }
+
+        public ConsoleColor ForegroundColor { get; }
+
+        public void SetToConsole()
+        {
+            Console.BackgroundColor = BackgroundColor;
+            Console.ForegroundColor = ForegroundColor;
+        }
+    }
+}
